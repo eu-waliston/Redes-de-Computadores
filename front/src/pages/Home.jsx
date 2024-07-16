cd427378e85f04a66274a167f9f3c17a3637bc39
Waliston <wesantosdev@gmail.com>
2024-07-16 17:57:08 -0300

    update

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -7,9 +7,6 @@ const Home = () => {
     return (
         <div className="home--component">
             <Menu />
-            <div className="greet">
-                <h1>Protocolos IPV4</h1>
-            </div>
             <video autoPlay loop muted id="video">
                 <source src={backgroundVideo} type="video/mp4"/>
             </video>
